8d8ee5fbc2fc1f1d2afe67f4037317353f6b79bb
Pauline BOURMEAU <pbourmea@e3r2p15.42.fr>
2016-10-10 18:45:29 +0200

    first window

---
+++ Coffee_machine/init.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+fenetre = Tk()
+
+champ_label = Label(fenetre, pady=12, bg="#800000", text="Hello, I'm Albert, your coffee machine today")
+
+
+
+if __name__ == '__main__':
+
+    champ_label.pack()
+
+    fenetre.mainloop()
